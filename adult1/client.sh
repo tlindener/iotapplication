ab3daaae6d90837a49b726d6b42ee8d666e0269b
tlindener <tobias.lindener@gmail.com>
2014-05-12 16:08:31 +0200

    Rewrite of client.sh

--- adult1/client.sh
+++ adult1/client.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 while true;
 do
-	while read line;
-	do
-    mosquitto_pub -h 192.168.5.5 -p 1883 -t /health/sensors/heartrate/adult1 -m "{"Person": "Adult1","Heartrate": $line,"Excercise": true}"
-	done < $1
-sleep 5
+for line in $(cat /root/data.txt);
+do
+   mosquitto_pub -h 192.168.5.5 -p 1883 -t /health/sensors/heartrate/adult1 -m "{"Person": "Adult1","Heartrate": $line,"Excercise": true}"
+sleep 5; done
 done
