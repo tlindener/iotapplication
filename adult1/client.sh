0244fa6837ba54ba9b7a2f399dd7a512a25545e3
tlindener <tobias.lindener@gmail.com>
2014-05-12 15:57:43 +0200

    Fixes in client.sh

--- adult1/client.sh
+++ adult1/client.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while true;
 do
-while read line
-do
+	while read line;
+	do
     mosquitto_pub -h 192.168.5.5 -p 1883 -t /health/sensors/heartrate/adult1 -m "{"Person": "Adult1","Heartrate": $line,"Excercise": true}"
-done < $1
+	done < $1
 sleep 5
 done
--- buildmqttclients.sh
+++ buildmqttclients.sh
@@ -4,7 +4,7 @@ docker build --rm -t tlindener/mqttclientadult1 .
 for i in {6..10}
 do
 	mqttserver=$(sudo docker run -d -P tlindener/mqttclientadult1)
-	cd /home/iot/iotframework
+	cd /home/iot/iotapplication
 	./pipework br1 $mqttserver 192.168.5.$i/24
 done
 cd /home/iot/iotapplication/adult2
@@ -12,7 +12,7 @@ docker build --rm -t tlindener/mqttclientadult2 .
 for i in {11..15}
 do
 	mqttserver=$(sudo docker run -d -P tlindener/mqttclientadult2)
-	cd /home/iot/iotframework
+	cd /home/iot/iotapplication
 	./pipework br1 $mqttserver 192.168.5.$i/24
 done
 
