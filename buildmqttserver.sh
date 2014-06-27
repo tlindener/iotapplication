e07a762b7fcb53759205a053ecbb0c62cfd2c231
tlindener <tobias.lindener@gmail.com>
2014-06-27 12:40:21 +0200

    Moved application test to python based framework

--- buildmqttserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/iot/iotapplication/mqttserver
-docker build --rm -t tlindener/mqttserver .
-mqttserver=$(sudo docker run -d -P --name mqttserver tlindener/mqttserver)
-cd /home/iot/iotapplication
-./pipework br1 $mqttserver 192.168.5.5/24
